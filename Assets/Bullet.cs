7f0d4484e008ace8b966bea82663f007a1782a31
Jeffrey Thor <jeffreythor@Jeffreys-MacBook-Pro.local>
2018-08-29 20:49:26 -0400

    Initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision) {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if (health != null) {
+            health.TakeDamage(10);
+        }
+        Destroy(gameObject);
+    }
+}
