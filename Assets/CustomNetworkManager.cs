5a4ea4b1c466bae304dcf591a8d715ddcfd56255
Jeffrey Thor <thorjeffrey@gmail.com>
2018-08-30 08:22:44 -0400

    Deletes auto-start as client on non headless builds

--- Assets/CustomNetworkManager.cs
+++ Assets/CustomNetworkManager.cs
@@ -2,7 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Rendering;
 
 public class CustomNetworkManager : NetworkManager {
+    // detect headless mode (which has graphicsDeviceType Null)
+    bool IsHeadless() {
+        return SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null;
+    }
 
+    private void Awake() {
+        if (IsHeadless()) {
+            print("headless mode detected");
+            StartServer();
+        }
+    }
 }
