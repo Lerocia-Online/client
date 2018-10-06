8860e9da39191069e06b4f7d6a7da13e9cc12957
Jeffrey Thor <thorjeffrey@gmail.com>
2018-10-05 22:56:33 -0400

    Removes quit and nographics flags from build command

--- Assets/Scripts/build.sh
+++ Assets/Scripts/build.sh
@@ -3,17 +3,11 @@
 project="Multiplayer Example"
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -quit 
-  -batchmode 
-  -nographics 
-  -executeMethod BuildHelper.build
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -executeMethod BuildHelper.build
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
 
 
 echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
+zip -r $(pwd)/Builds/server.zip $(pwd)/Builds/
