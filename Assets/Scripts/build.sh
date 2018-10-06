74b19d2a823b14386b698b3dd13a13764daafabd
Jeffrey Thor <thorjeffrey@gmail.com>
2018-10-05 23:06:19 -0400

    Adds build logging

--- Assets/Scripts/build.sh
+++ Assets/Scripts/build.sh
@@ -3,11 +3,10 @@
 project="Multiplayer Example"
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -executeMethod BuildHelper.build
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile $(pwd)/unity.log -executeMethod BuildHelper.build
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
-
 
 echo 'Attempting to zip builds'
 zip -r $(pwd)/Builds/server.zip $(pwd)/Builds/
