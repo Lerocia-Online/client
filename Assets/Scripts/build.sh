cc0109a15c6458f90dc26fb4beca21f16fac1362
Jeffrey Thor <thorjeffrey@gmail.com>
2018-10-05 23:20:51 -0400

    Adds nographics flag and silent crashes to build command

--- Assets/Scripts/build.sh
+++ Assets/Scripts/build.sh
@@ -3,7 +3,7 @@
 project="Multiplayer Example"
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -logFile $(pwd)/unity.log -executeMethod BuildHelper.build
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -executeMethod BuildHelper.build -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
