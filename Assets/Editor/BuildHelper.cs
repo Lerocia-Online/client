05801c87a8e1061f1cd7fb3353429cbba53cd339
Jeffrey Thor <thorjeffrey@gmail.com>
2018-10-05 23:52:18 -0400

    Changes build output location

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 class BuildHelper {
     static void build() {
         string[] scenes = {"Assets/Scenes/Main.unity"};
-        string pathToDeploy = "Builds/ServerBuild";
+        string pathToDeploy = "../../Builds/ServerBuild";
         BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneLinuxUniversal, BuildOptions.EnableHeadlessMode);
     }
 }
