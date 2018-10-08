1ecdcc37cfbcd887dc5bd9e7b5a459854b25a29e
Jeffrey Thor <thorjeffrey@gmail.com>
2018-10-07 20:17:52 -0400

    Removes logging from build helper

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -5,8 +5,6 @@ class BuildHelper {
     static void build() {
         string[] scenes = {"Assets/Scenes/Main.unity"};
         string pathToDeploy = "Builds/ServerBuild";
-        Debug.Log("**********PRE-BUILD**********");
         BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneLinuxUniversal, BuildOptions.EnableHeadlessMode);
-        Debug.Log("**********POST-BUILD**********");
     }
 }
