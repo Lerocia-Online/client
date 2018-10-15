1d53dbd3d1452bfc4a4b69f4d1615e57059c08c2
Jeffrey Thor <JeffreyThor@users.noreply.github.com>
2018-10-14 22:05:09 -0400

    Adds BuildHelper file (#22)
    * Adds BuildHelper for command line builds and travis CI config file and corresponding build/install bash scripts
    
    * Moves shell scripts
    
    * Updates build config to remove language specification
    
    * Adjusts path to shell scripts
    
    * Updates build config shell script paths
    
    * Removes extraneous backslash from build config
    
    * Updates path to shell scripts
    
    * Specifies os in travis config
    
    * Removes quit and nographics flags from build command
    
    * Adds build logging
    
    * Adds quit flad to build command
    
    * Adds nographics flag and silent crashes to build command
    
    * Fixes zip command
    
    * Changes build output location
    
    * Corrects build helper namespace
    
    * Reverts BuildHelper namespace
    
    * Resets manifest permissions
    
    * Removes travis config and bash files
    
    * adds logs to build helper
    
    * Removes logging from build helper

---
+++ Assets/Editor/BuildHelper.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+using UnityEngine;
+
+class BuildHelper {
+    static void build() {
+        string[] scenes = {"Assets/Scenes/Main.unity"};
+        string pathToDeploy = "Builds/ServerBuild";
+        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneLinuxUniversal, BuildOptions.EnableHeadlessMode);
+    }
+}
