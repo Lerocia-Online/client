be89c4f157ea38198c6e61cd973236acadfd4343
Jeffrey Thor <thorjeffrey@gmail.com>
2018-10-20 13:48:32 -0400

    Adds client builds to build helper

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -6,4 +6,14 @@ class BuildHelper {
         string pathToDeploy = "Builds/ServerBuild";
         BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneLinuxUniversal, BuildOptions.EnableHeadlessMode);
     }
+    static void buildWindowsClient() {
+        string[] scenes = {"Assets/Scenes/Main.unity"};
+        string pathToDeploy = "Builds/WindowsClientBuild";
+        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneWindows, BuildOptions.EnableHeadlessMode);
+    }
+    static void buildMacClient() {
+        string[] scenes = {"Assets/Scenes/Main.unity"};
+        string pathToDeploy = "Builds/MacClientBuild";
+        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneOSX, BuildOptions.EnableHeadlessMode);
+    }
 }
