d19a5721e172b0e43309a5d4427cc7ba737459e2
Jeffrey Thor <thorjeffrey@gmail.com>
2018-10-23 19:09:39 -0400

    Updates code styling

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -1,19 +1,22 @@
 using UnityEditor;
 
 class BuildHelper {
-    static void build() {
-        string[] scenes = {"Assets/Scenes/Main.unity"};
-        string pathToDeploy = "Builds/ServerBuild";
-        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneLinuxUniversal, BuildOptions.EnableHeadlessMode);
-    }
-    static void buildWindowsClient() {
-        string[] scenes = {"Assets/Scenes/Main.unity"};
-        string pathToDeploy = "Builds/WindowsClientBuild.exe";
-        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneWindows64, BuildOptions.None);
-    }
-    static void buildMacClient() {
-        string[] scenes = {"Assets/Scenes/Main.unity"};
-        string pathToDeploy = "Builds/MacClientBuild";
-        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneOSX, BuildOptions.None);
-    }
+  static void build() {
+    string[] scenes = {"Assets/Scenes/Main.unity"};
+    string pathToDeploy = "Builds/ServerBuild";
+    BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneLinuxUniversal,
+      BuildOptions.EnableHeadlessMode);
+  }
+
+  static void buildWindowsClient() {
+    string[] scenes = {"Assets/Scenes/Main.unity"};
+    string pathToDeploy = "Builds/WindowsClientBuild.exe";
+    BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneWindows64, BuildOptions.None);
+  }
+
+  static void buildMacClient() {
+    string[] scenes = {"Assets/Scenes/Main.unity"};
+    string pathToDeploy = "Builds/MacClientBuild";
+    BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.StandaloneOSX, BuildOptions.None);
+  }
 }
