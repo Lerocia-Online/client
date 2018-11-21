3b4ecb9ed3e9d6cf00e2bd43a4993d6df32cb54b
Jeffrey Thor <thorjeffrey@gmail.com>
2018-11-20 22:51:50 -0500

    Adds menu item for mac and windows builds

---
+++ Assets/Editor/BuildMenu.cs
@@ -0,0 +1,57 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+public class BuildMenu : MonoBehaviour {
+  [MenuItem("Build/Build Mac")]
+  public static void BuildMac() {
+    BuildPlayerOptions buildClientPlayerOptions = new BuildPlayerOptions();
+    buildClientPlayerOptions.scenes = new[] { "Assets/Scenes/Client.unity" };
+    buildClientPlayerOptions.locationPathName = "Builds/Client";
+    buildClientPlayerOptions.target = BuildTarget.StandaloneOSX;
+    buildClientPlayerOptions.options = BuildOptions.None;
+
+    Build(buildClientPlayerOptions);
+
+    BuildPlayerOptions buildServerPlayerOptions = new BuildPlayerOptions();
+    buildServerPlayerOptions.scenes = new[] { "../server/Assets/Scenes/Server.unity" };
+    buildServerPlayerOptions.locationPathName = "Builds/Server";
+    buildServerPlayerOptions.target = BuildTarget.StandaloneOSX;
+    buildServerPlayerOptions.options = BuildOptions.None;
+
+    Build(buildServerPlayerOptions);
+  }
+  
+  [MenuItem("Build/Build Windows")]
+  public static void BuildWindows() {
+    BuildPlayerOptions buildClientPlayerOptions = new BuildPlayerOptions();
+    buildClientPlayerOptions.scenes = new[] { "Assets/Scenes/Client.unity" };
+    buildClientPlayerOptions.locationPathName = "Builds/Client";
+    buildClientPlayerOptions.target = BuildTarget.StandaloneWindows64;
+    buildClientPlayerOptions.options = BuildOptions.None;
+
+    Build(buildClientPlayerOptions);
+
+    BuildPlayerOptions buildServerPlayerOptions = new BuildPlayerOptions();
+    buildServerPlayerOptions.scenes = new[] { "../server/Assets/Scenes/Server.unity" };
+    buildServerPlayerOptions.locationPathName = "Builds/Server";
+    buildServerPlayerOptions.target = BuildTarget.StandaloneWindows64;
+    buildServerPlayerOptions.options = BuildOptions.None;
+
+    Build(buildServerPlayerOptions);
+  }
+
+  private static void Build(BuildPlayerOptions buildPlayerOptions) {
+    BuildPipeline.BuildPlayer(buildPlayerOptions);
+    BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+    BuildSummary summary = report.summary;
+
+    if (summary.result == BuildResult.Succeeded) {
+      Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+    }
+
+    if (summary.result == BuildResult.Failed) {
+      Debug.Log("Build failed");
+    }
+  }
+}
