75c6e5bd159930e52a28714fd830f6983a5a77d7
Jeffrey Thor <JeffreyThor@users.noreply.github.com>
2019-01-18 11:26:27 -0500

    Adds a punching animation when attacking and developer UI (#57)
    * Fixes the ability to set player position on start
    
    * Updates canvas for screen size scaling
    
    * Adds play offline option when in editor
    
    * Adds forward punch visual action, only visible to current player
    
    * Alternates punching arms
    
    * Arms pull back more slowly after an attack
    
    * Formatting and removing logs
    
    * Moves player camera object to child of player
    
    * Adds call to attack function for networked players when their attack message is received
    
    * Adds time to attack request
    
    * Adds editor controls and converts attack to animation
    
    * Animation controller
    
    * Disables developer controls on non-editor builds
    
    * Moves animations into animation directory
    
    * Revert "Moves animations into animation directory"
    
    This reverts commit 8b1d463de7c9942f6ea53c29889924dc481091d1.
    
    * Revert "Disables developer controls on non-editor builds"
    
    This reverts commit 18f1eda798ebc092541386b5035a45d491f00ecf.
    
    * Removes logs
    
    * Converst player canvas to dev canvas for editor only mode

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class PlayerMotor : MonoBehaviour {
-  
   public float speed = 6.0f;
   public float jumpSpeed = 8.0f;
   public float gravity = 20.0f;
@@ -10,18 +9,15 @@ public class PlayerMotor : MonoBehaviour {
   private Vector3 moveDirection = Vector3.zero;
   private CharacterController controller;
 
-  void Start()
-  {
+  void Start() {
     controller = GetComponent<CharacterController>();
 
     // let the gameObject fall down
-    gameObject.transform.position = new Vector3(0, 5, 0);
+    gameObject.transform.position = new Vector3(transform.position.x, 5, transform.position.z);
   }
 
-  void Update()
-  {
-    if (controller.isGrounded)
-    {
+  void Update() {
+    if (controller.isGrounded) {
       // We are grounded, so recalculate
       // move direction directly from axes
 
@@ -29,8 +25,7 @@ public class PlayerMotor : MonoBehaviour {
       moveDirection = transform.TransformDirection(moveDirection);
       moveDirection = moveDirection * speed;
 
-      if (Input.GetButton("Jump"))
-      {
+      if (Input.GetButton("Jump")) {
         moveDirection.y = jumpSpeed;
       }
     }
